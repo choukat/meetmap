1bb640bd26dae0f1e2bbb0485f42e08f12cf7ded
choukat <48254491+choukat@users.noreply.github.com>
2019-04-07 23:34:12 +0200

    Update googleMapAPI.js

--- API/googleMapAPI.js
+++ API/googleMapAPI.js
@@ -1,6 +1,6 @@
 // API/googleMapAPI.js
 
-const API_KEY='AIzaSyCgHcYHXKUgOXyXyed_AyaNC8xlZLpq5qg'
+const API_KEY='SECRET'
 
 export function getMapData() {
   const url='https://maps.googleapis.com/maps/api/directions/json?origin=41.13694,-73.359778&destination=41.13546,-73.35997&mode=driver&sensor=true&key='+API_KEY
